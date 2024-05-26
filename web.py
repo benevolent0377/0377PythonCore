24c675c237c475b43f72dd4eda9a577c8481b9f1
calithos4136 <149543612+calithos4136@users.noreply.github.com>
2024-05-26 18:12:12 -0500

    cleaned up web.py

--- web.py
+++ web.py
@@ -1,11 +1,5 @@
 import socket
-import ftplib
-from . import IO, log
-
-# a file for all the HTTP and HTTPS functions
-hostName = "calithos.in"
-usr = "wanderer@calithos.in"
-pwd = "USYuQPD41"
+from . import log
 
 def ping(host="8.8.8.8", port=53, timeout=10):
     """
@@ -20,48 +14,3 @@ def ping(host="8.8.8.8", port=53, timeout=10):
     except socket.error as ex:
         log.log(ex, "err")
         return False
-
-def fetchFTP(localFile, remoteFile, timeout=45):
-    IO.say("Establishing FTP connection...\n")
-    with ftplib.FTP(hostName, timeout=timeout) as ftp:
-        ftp.login(user=usr, passwd=pwd)
-        log.log(hostName, "ftpconnect", usr)
-        ftp.encoding = "utf-8"
-        with open(localFile, "wb") as file:
-            ftp.retrbinary(f"RETR {remoteFile}", file.write)
-            log.log(remoteFile, "ftpD", hostName)
-            IO.say("Done.")
-
-
-def postFTP(localFile, remoteFile, timeout=45, isTmp=False, isYaml=False, keyword=""):
-    with ftplib.FTP(hostName, timeout=timeout) as ftp:
-        ftp.login(user=usr, passwd=pwd)
-        log.log(hostName, "ftpconnect", usr)
-        ftp.encoding = "utf-8"
-        if isTmp:
-
-            with open(localFile, "xw") as file:
-                if isYaml:
-                    data = IO.yamlRead(localFile, keyword)
-                    file.write(data)
-                else:
-                    file = IO.fileRead(localFile)
-
-                ftp.storlines(f"STOR {remoteFile}", file)
-                log.log(remoteFile, "ftpD", hostName)
-
-        else:
-
-            with open(localFile, "xw") as file:
-                if isYaml:
-                    data = IO.yamlRead(localFile, keyword)
-                    file.write(data)
-                else:
-                    file = IO.fileRead(localFile)
-
-                ftp.storlines(f"STOR {remoteFile}", file)
-                log.log(remoteFile, "ftpD", hostName)
-
-def HTTPget(host, destination, timeout=45):
-    # requests.get() <-- incomplete command + filler code
-    print()
