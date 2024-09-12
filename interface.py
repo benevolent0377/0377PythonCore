7e9de7d255660320caed362f359a04699b40f56f
calithos4136 <calithos4136@gmail.com>
2024-09-12 17:05:20 -0500

    updated interface.py

--- interface.py
+++ interface.py
@@ -1,4 +1,6 @@
 # a file that handles all things related to the guis for a program
-import tkinter as gui
 
-# there's nothing here yet, yet
+# from tkinter import *
+# from tkinter import ttk
+
+# currently being implemented, not yet complete
